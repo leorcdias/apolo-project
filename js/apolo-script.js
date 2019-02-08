f13928bd494eb8ec18d71e08ab72262bdbf89afb
Leonardo Dias <leorcdias@hotmail.com>
2019-02-08 01:30:26 -0200

    Scroll ID Page Function v3

--- js/apolo-script.js
+++ js/apolo-script.js
@@ -65,11 +65,11 @@ $(document).ready(function () {
 function hashSamePage() {
 	$('a[href^="#"]').on('click', function (e) {
 		e.preventDefault();
-		var id = $(this).attr('href'),
+		let id = $(this).attr('href'),
 			targetOffset = $(id).offset().top;
 		$('html, body').animate({
-			scrollTop: targetOffset.offset().top
-		}, 700, 'swing');
+			scrollTop: targetOffset - 100
+		}, 1000);
 	});
 }
 
